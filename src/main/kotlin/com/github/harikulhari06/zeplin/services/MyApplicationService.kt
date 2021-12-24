d9aead534590a8c09cc54c19091df0be17183565
GitHub Action <action@github.com>
2021-12-24 04:48:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/harikulhari06/zeplin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/harikulhari06/zeplin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.harikulhari06.zeplin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.harikulhari06.zeplin.MyBundle
 
 class MyApplicationService {
 
